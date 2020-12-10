2dfe86caa08151d510b9e674484da571d28d533b
algertc <algertc@gmail.com>
2020-12-10 15:40:01 -0800

    pywinauto update

--- pywinautoWORKING_BACKUP_ZOOM.py
+++ pywinautoWORKING_BACKUP_ZOOM.py
@@ -1,12 +1,15 @@
 from pywinauto import Desktop, Application
+
+#todo this should use try except in case the sign in is not required
+
 app = Application(backend="uia").start("zoom --url=https://auhsdschools.zoom.us/j/7753695672?pwd=NTB1V1FRYTRnalBlYlBidnhXdDd2dz09")
 
 #dialog
 dialog = Desktop(backend="uia").window(title="Zoom Cloud Meetings", top_level_only=False)
 dialog.print_control_identifiers()
 
-signIn = dialog.child_window(title="Sign In", auto_id="Button5", control_type="Button")
-signIn.click()
+#signIn = dialog.child_window(title="Sign In", auto_id="Button5", control_type="Button")
+#signIn.click()
 #clicks
 #dialog.child_window(title="Sign In", auto_id="Sign InButton", control_type="Button").click()
 
