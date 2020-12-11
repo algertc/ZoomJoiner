86bb60c90994bf81baa0c242f449c46dc99a21e3
algertc <algertc@gmail.com>
2020-12-10 16:00:02 -0800

    programatically sign into zoom

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import Join
 import time
 import configparser
+import subprocess
 
 
 #load the config
@@ -26,11 +27,32 @@ Finals_Week1 = ["12/15/20", "12/16/20", "12/17/20", "12/18/20"]
 Finals_Week2 = ["5/24/21", "5/25/21", "5/26/21", "5/27/21", "5/28/21"]
 #------------------------------------------------------------------------------------------------------------------------------####
 
+def login():
+    #Programatically signs into zoom using the api
+    from pywinauto import Desktop, Application
+    app = Application(backend="uia").start("zoom --login")
+    main_win = app.window(title='Zoom Cloud Meetings')
+    signIn = main_win.child_window(title="Sign In", control_type="Button")
+    try:
+        signIn.click()
+        time.sleep(0.3)
+    except:
+        try:
+            signInWithGoogle = main_win.child_window(title="Sign In with Google", control_type="Button")
+            signInWithGoogle.click()
+
+        except:
+            print("SigninError")
+    #main_win.print_control_identifiers()
+login()
+
 def maain():
     while True:
         #store the date
         date = datetime.now().strftime("%D")
 
+        #todo make sure the user is logged in
+        #login()
 
     #make sure today is not a holiday or final day
         if date in Holidays == False and date in Finals_Week1 == False and date in Finals_Week2 == False:
